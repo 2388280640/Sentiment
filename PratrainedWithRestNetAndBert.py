db96fe76fce6edd6f13995933e96e4cf9d60cea0
Elizabeth <75080626+2388280640@users.noreply.github.com>
2023-05-11 10:37:34 +0800

    Update and rename MyProject.py to PratrainedWithRestNetAndBert.py

--- PratrainedWithRestNetAndBert.py
+++ PratrainedWithRestNetAndBert.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import numpy as np
@@ -22,6 +21,7 @@ from transformers import BertTokenizer,TFBertModel,BertModel
 from keras import optimizers
 import tensorflow as tf
 import json
+from keras.callbacks import EarlyStopping #提前停止训练
 #https://blog.csdn.net/yjw123456/article/details/120232707
 class LayerPlus(Layer):
     def get_config(self):
@@ -169,37 +169,58 @@ def ConverToNumpy(tensor):
         cls=sess.run(tensor)
     return cls
 def PreProcessWithBert(data):
-    bert_model = TFBertModel.from_pretrained('../bert-base-uncased')
-    tokenizer = BertTokenizer.from_pretrained('../bert-base-uncased')
+    bert_model = TFBertModel.from_pretrained('./bert-base-uncased')
+    tokenizer = BertTokenizer.from_pretrained('./bert-base-uncased')
     bertOutput1 = None
     bertOutput2 = None
     token = tokenizer(data[:7000],return_tensors='tf',max_length=50,padding='max_length',truncation=True,add_special_tokens=True)
+
     output = bert_model(token)
     with tf.compat.v1.Session() as sess:
         sess.run(tf.compat.v1.global_variables_initializer())
         bertOutput1 = sess.run(output['last_hidden_state'])
-    print(bertOutput1.shape)
     token = tokenizer(data[7000:], return_tensors='tf', max_length=50, padding='max_length', truncation=True,
                       add_special_tokens=True)
+
     output = bert_model(token)
     with tf.compat.v1.Session() as sess:
         sess.run(tf.compat.v1.global_variables_initializer())
         bertOutput2 =sess.run(output['last_hidden_state'])
-    print(bertOutput2.shape)
-    return np.concatenate((bertOutput1,bertOutput2),axis=0)
+
+    pretrained_data = np.concatenate((bertOutput1,bertOutput2),axis=0)
+    np.save('text_data_pretrained.npy',pretrained_data)
+    return output
 
 def createModel(train_data):
+    
+    '''
     inputLayer = Input(shape=(train_data.shape[1], train_data.shape[2]))
     batchNormalization = BatchNormalization()(inputLayer)
     dense1 = Dense(units=64, activation='relu')(batchNormalization)
     dropout1 = Dropout(0.3)(dense1)
-    pool1 = MaxPooling1D(pool_size=5,strides=None,padding='valid')(dropout1)
+    pool1 = AveragePooling1D(pool_size=5,strides=None,padding='valid')(dropout1)
     dense2 = Dense(units=128,activation='relu')(pool1)
     dropout2 = Dropout(0.3)(dense2)
     fla = Flatten()(dropout2)
     OutputLayer = Dense(units=3, activation='softmax')(fla)
     model = Model(inputs=inputLayer, outputs=OutputLayer)
     sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.001)
+    #adam = optimizers.adam_v2.Adam(learning_rate=0.001)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.summary()
+    return model
+    '''
+    inputLayer = Input(shape=(train_data.shape[1], train_data.shape[2]))
+    batchNormalization = BatchNormalization()(inputLayer)
+    dense1 = Dense(units=64, activation='relu')(batchNormalization)
+    dropout1 = Dropout(0.3)(dense1)
+    dense2 = Dense(units=128,activation='relu')(dropout1)
+    dropout2 = Dropout(0.3)(dense2)
+    pool1 = AveragePooling1D(pool_size=5,strides=None,padding='valid')(dropout2)
+    fla = Flatten()(pool1)
+    OutputLayer = Dense(units=3, activation='softmax')(fla)
+    model = Model(inputs=inputLayer, outputs=OutputLayer)
+    sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.01)
     #adam = optimizers.adam_v2.Adam(learning_rate=0.001)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.summary()
@@ -215,21 +236,25 @@ def show_train_history(history,train,validation):
     plt.legend(['train','validation'],loc='upper left')
     plt.show()
 
+#最好的效果参数epochs=200,
+
 def TrainModel(model,train_data,train_label,test_data,test_label):
+    earlyStop = EarlyStopping(monitor='val_accuracy',min_delta=0,patience=100,mode='max',verbose=1,restore_best_weights=True)
     histoty=model.fit(
         x=train_data,
         y=train_label,
-        batch_size=32,
-        epochs=100,
+        batch_size=120,
+        callbacks=[earlyStop],
+        epochs=500,
         validation_split=0.2,
-        verbose=1
+        verbose=1,
+        shuffle=True
     )
-    model.save_weights('BertRestNet503.h5')
+    model.save_weights('BertRestNet50FineTune.h5')
     score= model.evaluate(x=test_data,y=test_label)
     print(score[1])
-    with open('BertRestNet50SGD3.json','w') as f:
+    with open('BertRestNet50FineTune.json','w') as f:
         json.dump(histoty.history,f)
-   # show_train_history(histoty, 'loss', 'val_loss')
 
 def TestModel(model,test_data,test_label):
     score = model.evaluate(x=test_data,y=test_label)
@@ -241,26 +266,32 @@ def CreateBiLSTMModel(train_data):
     BiLSTM = Bidirectional(LSTM(train_data.shape[1], return_sequences=True), merge_mode='concat',
                            input_shape=(train_data.shape[1], train_data.shape[2]))(batchNormalization)
     AttentionLayer = MultiAttention(out_dim=train_data.shape[1]*2, heads=8)(BiLSTM)
-    fla = Flatten()(AttentionLayer)
+    dropout1 = Dropout(0.3)(AttentionLayer)
+    fla = Flatten()(dropout1)
     OutputLayer = Dense(units=3, activation='softmax')(fla)
     model = Model(inputs=inputLayer, outputs=OutputLayer)
-    sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.001)
+    sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.01)
+    #adam = optimizers.adam_v2.Adam(learning_rate=0.001)
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     model.summary()
     return model
 
 def TrainBiLSTMModel(model,train_data,train_label,test_data,test_label):
+    earlyStop = EarlyStopping(monitor='val_accuracy',min_delta=0,patience=100,mode='max',verbose=1,restore_best_weights=True)
     histoty = model.fit(
         x=train_data,
         y=train_label,
-        batch_size=64,
-        epochs=20,
-        validation_split=0.2,
-        verbose=1
+        callbacks=[earlyStop],
+        batch_size=80,
+        epochs=500,
+        validation_split=0.3,
+        verbose=1,
+        shuffle=True
     )
     model.save_weights('BiLSTM.h5')
-    score=model.evaluate(x=test_data,y=test_label)
-    print(score[1])
+    score = model.evaluate(x=test_data,y=test_label)
+    print("BiLSTM accuracy:",score[1])
+    return model
 
 
 
@@ -270,6 +301,8 @@ def TrainBiLSTMModel(model,train_data,train_label,test_data,test_label):
 from transformers import BeitImageProcessor,BeitForImageClassification
 from PIL import Image
 from keras.applications.resnet import ResNet50
+from keras.applications.vgg19 import VGG19
+import keras.applications.vgg19 as vgg19
 from keras.applications.resnet import  preprocess_input
 def readfile(path):
     label_map = {'positive': 0,
@@ -319,54 +352,187 @@ def readfile(path):
 
     return text_data,image_path,label
 
-def extractFromBiLSTM(train_data):
-    BiLSTM = CreateBiLSTMModel(train_data)
-    BiLSTM.load_weights('BiLSTM.h5')
+def RestNetFineTune(image_path,label):
+    imagelist=[]
+    label_Onehot = []
+    if os.path.exists('image_input.npy') and os.path.exists('label_finetune_restnet.npy'):
+        imagelist= np.load('image_input.npy')
+        label_Onehot = np.load('label_finetune_restnet.npy')
+    else:
+        np.random.seed(999)
+        np.random.shuffle(image_path)
+        np.random.seed(999)
+        np.random.shuffle(label)
+        for path in image_path:
+            img = Image.open(path)
+            img = img.resize((224,224),Image.ANTIALIAS)
+            x = np.array(img)
+            #print(x.shape)
+            imagelist = imagelist+[preprocess_input(x)]
+        imagelist= np.array(imagelist)
+        label_Onehot = np_utils.to_categorical(label)
+        np.save('image_input.npy',imagelist)
+        np.save('label_finetune_restnet.npy',label_Onehot)
+    seed=np.random.randint(0,100)
+    np.random.seed(seed)
+    np.random.shuffle(imagelist)
+    np.random.seed(seed)
+    np.random.shuffle(label_Onehot)
+    train_data=imagelist[:9000]
+    train_label=label_Onehot[:9000]
+    test_data=imagelist[9000:]
+    test_label=label_Onehot[9000:]
+    #imagenet
+    rest50 = ResNet50(weights='imagenet',include_top=True,input_shape=(224,224,3))
+    x=rest50.layers[-1].input
+    outputLayer = Dense(units=3,activation='softmax')(x)
+    model=Model(inputs=rest50.input,outputs=outputLayer)
+    #print(len(model.layers))
+    mid = int(len(model.layers)*80/100)
+    for i in range(mid):
+        model.layers[i].trainable=False
+    sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.001)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.fit(x=train_data,y=train_label,validation_split=0.5,batch_size=120,epochs=50,verbose=1)
+    scores=model.evaluate(x=test_data,y=test_label)
+    print(scores[1])
+    model.save_weights('RestNet50.h5')
+    return model
+
+def VGG19FineTune(image_path,label):
+    imagelist=[]
+    label_Onehot = []
+    if os.path.exists('image_input.npy') and os.path.exists('label_finetune.npy'):
+        imagelist= np.load('image_input.npy')
+        label_Onehot = np.load('label_finetune.npy')
+    else:
+        np.random.seed(999)
+        np.random.shuffle(image_path)
+        np.random.seed(999)
+        np.random.shuffle(label)
+        for path in image_path:
+            img = Image.open(path)
+            img = img.resize((224, 224))
+            x = np.array(img)
+            imagelist = imagelist+[preprocess_input(x)]
+        imagelist= np.array(imagelist)
+        label_Onehot = np_utils.to_categorical(label)
+        np.save('image_input.npy',imagelist)
+        np.save('label_finetune.npy',label_Onehot)
+    train_data=imagelist[:8000]
+    train_label=label_Onehot[:8000]
+    test_data=imagelist[8000:]
+    test_label=label_Onehot[8000:]
+
+    vgg = VGG19(weights='imagenet',include_top=True,input_shape=(224,224,3))
+    vggoutput = vgg.layers[-1].input
+    outputLayer = Dense(units=3,activation='softmax')(vggoutput)
+    sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.005)
+    model=Model(inputs=vgg.input,outputs=outputLayer)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.fit(x=train_data,y=train_label,validation_data=[test_data,test_label],batch_size=32,epochs=50,verbose=1)
+    model.save_weights('Vgg19.h5')
+    return model
+
+def extractFromBiLSTM(BiLSTM,train_data):
     model = Model(inputs=BiLSTM.input, outputs=BiLSTM.get_layer('multi_attention').output)
     feature=model.predict(train_data)
-    return np.array(feature)
+    feature=np.array(feature)
+    np.save('text_data_extract_BiLSTM.npy',feature)
+    return feature
 
-def PreprocessImage(pathLit):
-    res50 = ResNet50(weights='imagenet',   include_top=True, input_shape=(224, 224, 3))
-    model = Model(inputs=res50.input, outputs=res50.get_layer('avg_pool').output)
+def PreprocessImage(model,pathList):
     picFeatureList=[]
-    for Item in pathLit:
-        picFeatureList=picFeatureList+[PicturePretrained(model, Item)]
+    for Item in pathList:
+        feature=None
+        img = Image.open(Item)
+        img = img.resize((224, 224))
+        x = np.array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        feature = model.predict(x)
+        feature = np.resize(feature, (50, 41))  # 原本尺寸为2048=2048,改变形状为50X41=2050
+        picFeatureList=picFeatureList+[feature]
     return np.array(picFeatureList)
 
-def PicturePretrained(model,path):
-    img = Image.open(path)
-    img = img.resize((224, 224))
-    x = np.array(img)
-    x = np.expand_dims(x,axis=0)
-    x = preprocess_input(x)
-    features = model.predict(x)
-    features=np.resize(features,(50,41))#原本尺寸为2048=2048,改变形状为50X41=2050
-    return features
-def BertBiLSTMRun():
-    label = np.load('label.npy')
-    image_pretrained=np.load('image_pretrained.npy')
-    text_data_extract_BiLSTM=np.load('text_data_extract_BiLSTM.npy')
+
+def ProjectRun():
+    text_data, image_path, label = readfile("MVSA")
+    image_pretrained = None
+    if os.path.exists('image_pretrained.npy') == False:
+        model  = None
+        if os.path.exists('RestNet50.h5') == False:
+            model = RestNetFineTune(image_path.copy(),label.copy())
+        else:
+            rest = ResNet50(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
+            x=rest.layers[-1].input
+            outputLayer = Dense(units=3,activation='softmax')(x)
+            model=Model(inputs=rest.input,outputs=outputLayer)
+            model.load_weights('RestNet50.h5')
+        image_pretrained=PreprocessImage(model,image_path)
+        np.save('image_pretrained.npy',image_pretrained)
+    else:
+        image_pretrained = np.load('image_pretrained.npy')
+    
+
+    text_data_pretrained=None
+    if os.path.exists('text_data_pretrained.npy')==False:
+        text_data_pretrained=PreProcessWithBert(text_data)
+    else:
+        text_data_pretrained=np.load('text_data_pretrained.npy')
+    print(type(text_data_pretrained))
+    BiLSTM=None
+    if os.path.exists('BiLSTM.h5') == False:
+        model = CreateBiLSTMModel(text_data_pretrained)
+        tp=text_data_pretrained.copy()
+        lb=label.copy()
+        np.random.seed(999)
+        np.random.shuffle(tp)
+        np.random.seed(999)
+        np.random.shuffle(lb)
+        label_Onehot = np_utils.to_categorical(lb)
+        train_data = tp[:8000]
+        test_data = tp[8000:]
+        train_label = label_Onehot[:8000]
+        test_label = label_Onehot[8000:]
+        BiLSTM=TrainBiLSTMModel(model,train_data,train_label,test_data,test_label)
+    else:
+        BiLSTM =CreateBiLSTMModel(text_data_pretrained)
+        BiLSTM.load_weights('BiLSTM.h5')
+    
+    text_data_extract_BiLSTM=None
+    if os.path.exists('text_data_extract_BiLSTM.npy') ==False:
+        text_data_extract_BiLSTM=extractFromBiLSTM(BiLSTM,text_data_pretrained)
+    else:
+        text_data_extract_BiLSTM=np.load('text_data_extract_BiLSTM.npy')
+
     concated_data=np.concatenate(( text_data_extract_BiLSTM ,image_pretrained),axis=2)
-    index = [i for i in range(len(concated_data))]
-    concated_data=concated_data[index]
-    label = label[index]
+    np.random.seed(999)
+    np.random.shuffle(concated_data)
+    np.random.seed(999)
+    np.random.shuffle(label)
     label_Onehot = np_utils.to_categorical(label)
     train_data=concated_data[:8000]
     test_data=concated_data[8000:]
     train_label =label_Onehot[:8000]
     test_label = label_Onehot[8000:]
-    print("train data shape: ",train_data.shape)
-    print("train label shape: ", train_label.shape)
-    print("test data shape: ", test_data.shape)
-    print("test label shape: ", test_label.shape)
     model=createModel(train_data)
-    #plot_model(model,'ResNet50_Bert_Multiattention.png',show_shapes=True)
-    #model.load_weights('BertRestNet502.h5')
-    #score = model.evaluate(x=test_data,y=test_label)
-    #print(score[1])
     TrainModel(model,train_data,train_label,test_data,test_label)
-
-BertBiLSTMRun()
+    
 #print(tf.test.is_gpu_available())
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ProjectRun()
+'''
+rest = ResNet50(weights='imagenet', include_top=True, input_shape=(224, 224, 3))
+x=rest.layers[-1].input
+outputLayer = Dense(units=3,activation='softmax')(x)
+model=Model(inputs=rest.input,outputs=outputLayer)
+sgd = optimizers.gradient_descent_v2.SGD(learning_rate=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+model.load_weights('RestNet50.h5')
+test_data=np.load('image_input.npy')
+test_label = np.load('label_finetune.npy')
+score=model.evaluate(x=test_data[3000:6000],y=test_label[3000:6000])
+print(score[1])
+'''
+#print(tf.test.is_gpu_available())
+#model accuracy:69.68%
